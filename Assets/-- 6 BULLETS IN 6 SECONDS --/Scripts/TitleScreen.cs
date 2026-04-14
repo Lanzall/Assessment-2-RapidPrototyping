5c57325f91c4c6e0422516e8fd3b5b5bbfe2c659
Lanzall <152286894+Lanzall@users.noreply.github.com>
2026-04-14 14:46:43 +1000

    TitleScreen Buttons and fade

--- Assets/-- 6 BULLETS IN 6 SECONDS --/Scripts/TitleScreen.cs
+++ Assets/-- 6 BULLETS IN 6 SECONDS --/Scripts/TitleScreen.cs
@@ -1,19 +1,74 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEditor.Timeline.Actions;
+using System.Collections;
 
 public class TitleScreen : MonoBehaviour
 {
-    //Referencing button for scene transition
+    [Header("References")]
+    public Animator howToPlayAnim;
+    public Animator fadeController;
+    public Canvas mainCanvas;
+    public Canvas fadeCanvas;
 
-    
-    void Start()
+
+    public InputAction closeWindowAction;
+
+    private bool helpWindowOpen;
+
+    private void OnEnable()
     {
-        
+        closeWindowAction.Enable();
+        closeWindowAction.performed += OnCloseWindow;
+
+        howToPlayAnim.SetTrigger("isClosed");
+        helpWindowOpen = false;
     }
 
     void Update()
     {
         
+    }
+
+    public void FadeComplete()
+    {
+        fadeCanvas.enabled = false;
+    }
+
+    public void StartGame()
+    {
+        fadeCanvas.enabled = true;
+        StartCoroutine(StartingGame("6Bullets"));
+    }
+
+    private IEnumerator StartingGame(string sceneName)
+    {
+        fadeController.SetTrigger("FadeEnable");
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void HowToPlay()
+    {
+        howToPlayAnim.SetTrigger("isOpened");
+        helpWindowOpen = true;
+        mainCanvas.enabled = false;
+    }
+
+    public void OnCloseWindow(InputAction.CallbackContext context)
+    {
+        if (!helpWindowOpen) return;
+        howToPlayAnim.SetTrigger("isClosed");
+        helpWindowOpen = false;
+        mainCanvas.enabled = true;
+    }
+
+    public void QuitToMenu()
+    {
+        Application.Quit();
     }
 }

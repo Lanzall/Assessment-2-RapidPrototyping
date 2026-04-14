6274271eab129cdf887520f3b2c3e15ed2a72dee
Lanzall <152286894+Lanzall@users.noreply.github.com>
2026-04-14 14:05:24 +1000

    TitleScreen start

---
+++ Assets/-- 6 BULLETS IN 6 SECONDS --/Scripts/TitleScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+    //Referencing button for scene transition
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

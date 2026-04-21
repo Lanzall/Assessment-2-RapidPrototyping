6adc5a5501a78d85e5b737d27a3c0888c0fc5109
Lanzall <benrock111e@gmail.com>
2026-04-21 16:49:55 +1000

    TitleScreen

---
+++ Assets/-- TITLE SCREEN --/Scripts/LevelSelector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    [Header("References")]
+    public CameraManager cameraManager;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
